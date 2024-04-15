fbf361b3757798e9e6c90b83a51d4b2c2113862f
Jason Volk <jason@zemos.net>
2024-04-14 23:33:24 -0700

    Allow setting logging callback.
    Signed-off-by: Jason Volk <jason@zemos.net>

--- tests/test_rocksdb_options.rs
+++ tests/test_rocksdb_options.rs
@@ -404,3 +404,20 @@ fn test_set_blob_cache() {
     // Cache hit
     assert_eq!(&*db.get(KEY).unwrap().unwrap(), VALUE);
 }
+
+#[test]
+fn test_set_callback_logger() {
+    use rust_rocksdb::LogLevel::Debug;
+    let path = DBPath::new("_set_callback_logger");
+    let mut msgs = 0;
+    {
+        let mut opts = Options::default();
+        opts.create_if_missing(true);
+        opts.set_callback_logger(Debug, &mut |_lev, _msg| {
+            msgs += 1;
+        });
+
+        let _db = DB::open(&opts, &path).unwrap();
+    }
+    assert!(msgs > 0, "callback logger produced no messages!");
+}
